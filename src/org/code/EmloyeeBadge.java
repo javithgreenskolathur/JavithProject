4516601a28ab0965bfc94e89e8d111e86d9e9077
javithgreenskolathur <javithgreens@gmail.com>
2025-05-24 17:37:16 +0530

    My Project

---
+++ src/org/code/EmloyeeBadge.java
@@ -0,0 +1,22 @@
+package org.code;
+
+public class EmloyeeBadge {
+
+	private void empBadge() {
+		System.out.println("123456");
+	}
+
+	private void empId() {
+		System.out.println("09876");
+	}
+	//end
+	public static void main(String[] args) {
+
+		EmloyeeBadge karthi = new EmloyeeBadge();
+		EmloyeeBadge vignesh = new EmloyeeBadge();
+		karthi.empBadge();
+		vignesh.empBadge();
+
+	}
+
+}
